f707b61996115000fe9a33dc12f3cca86b770956
何震震 <936432896@qq.com>
2015-12-01 17:09:18 +0800

    first commit

---
+++ SegyReader/SegyReader.h
@@ -0,0 +1,19 @@
+#ifndef __SEGYREADER_H__
+#define __SEGYREADER_H__
+
+
+
+
+
+// singleton pattern
+class SegyReader{
+private:
+	static SegyReader			*instance;
+
+private:
+	SegyReader(){}
+
+public:
+
+}; 
+#endif
